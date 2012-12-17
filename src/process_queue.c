26ebda8072b1e9563353d3c6832743999ebe43b9
Sander Demeester <demeester.sander@gmail.com>
2012-12-17 11:30:53 +0100

    add pthread code to process_queue

--- src/process_queue.c
+++ src/process_queue.c
@@ -4,7 +4,17 @@
 
 void *process_queue(void*ptr){
   volatile pthread_context*pcontext = (pthread_context*)ptr;
+  queue*q = pcontext->q;
   while(1){
+    //neem de mutex en controleer of er werk is voor ons
+    pthread_mutex_lock(pcontext->mutex);
+    if(q->number_of_elements>0){
+      //er is werk
+    }else{
+      //er is geen werk
+      pthread_cond_signal(pcontext->conditie);
+      pthread_mutex_unlock(pcontext->mutex);
+    }
     sleep(10);
   }
 }
